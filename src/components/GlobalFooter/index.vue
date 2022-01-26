52743be2a95ef044ccfec72186fc751b6c8b5f2d
杜劲松 <farben_dujinsong@farben.com>
2022-01-26 17:43:25 +0800

    'init'

---
+++ src/components/GlobalFooter/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <global-footer class="footer custom-render">
+    <template v-slot:links>
+    </template>
+    <template v-slot:copyright>
+      skeleton-admin
+    </template>
+  </global-footer>
+</template>
+
+<script>
+import { GlobalFooter } from '@ant-design-vue/pro-layout'
+
+export default {
+  name: 'ProGlobalFooter',
+  components: {
+    GlobalFooter
+  }
+}
+</script>
